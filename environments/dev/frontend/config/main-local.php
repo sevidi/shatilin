5901d62dc33a673ad162b5f2306911e92abd7f06
Victor Smirnov <istern24@mail.ru>
2019-04-10 14:53:20 +0300

    Unified authentication

--- environments/dev/frontend/config/main-local.php
+++ environments/dev/frontend/config/main-local.php
@@ -1,24 +1,21 @@
 <?php
 
-$config = [
-    'components' => [
-        'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
-        ],
-    ],
-];
+$config = [];
 
 if (!YII_ENV_TEST) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost
+        'allowedIPs' => ['127.0.0.1', '*'],
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost
+        'allowedIPs' => ['127.0.0.1', '*'],
     ];
 }
 
