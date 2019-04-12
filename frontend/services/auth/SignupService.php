3efc23795f61e84ff1a09349daefea77b36f1cfa
Victor Smirnov <istern24@mail.ru>
2019-04-12 19:36:39 +0300

    Extracted user repository

--- frontend/services/auth/SignupService.php
+++ frontend/services/auth/SignupService.php
@@ -5,11 +5,13 @@ namespace frontend\services\auth;
 
 use common\entities\User;
 use frontend\forms\SignupForm;
+use common\repositories\UserRepository;
 use yii\mail\MailerInterface;
 
 class SignupService
 {
     private $mailer;
+    private $users;
 
     public function __construct(MailerInterface $mailer)
     {
@@ -24,7 +26,7 @@ class SignupService
             $form->email,
             $form->password
         );
-        $this->save($user);
+        $this->users->save($user);
 
         $sent = $this->mailer
             ->compose(
@@ -45,24 +47,9 @@ class SignupService
             throw new \DomainException('Empty confirm token.');
         }
 
-        $user = $this->getByEmailConfirmToken($token);
+        $user = $this->users->getByEmailConfirmToken($token);
         $user->confirmSignup();
-        $this->save($user);
-    }
-
-    private function getByEmailConfirmToken(string $token): User
-    {
-        if (!$user = User::findOne(['email_confirm_token' => $token])) {
-            throw new \DomainException('User is not found.');
-        }
-        return $user;
-    }
-
-    private function save(User $user): void
-    {
-        if (!$user->save()) {
-            throw new \RuntimeException('Saving error.');
-        }
+        $this->users->save($user);
     }
 
 }
