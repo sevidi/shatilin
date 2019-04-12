922a91d00c126ccc7c47339ad81f15457b724ada
Victor Smirnov <istern24@mail.ru>
2019-04-12 19:52:00 +0300

    Added auth service

--- common/tests/unit/forms/LoginFormTest.php
+++ common/tests/unit/forms/LoginFormTest.php
@@ -5,17 +5,18 @@ namespace common\tests\unit\forms;
 
 use Yii;
 use common\forms\LoginForm;
-use common\fixtures\UserFixture as UserFixture;
+use common\fixtures\UserFixture;
+use Codeception\Test\Unit;
 /**
  * Login form test
  */
-
-class LoginFormTest extends  \Codeception\Test\Unit
+class LoginFormTest extends Unit
 {
     /**
      * @var \common\tests\UnitTester
      */
     protected $tester;
+
     public function _before()
     {
         $this->tester->haveFixtures([
@@ -25,34 +26,22 @@ class LoginFormTest extends  \Codeception\Test\Unit
             ]
         ]);
     }
-    public function testLoginNoUser()
+
+    public function testBlank()
     {
         $model = new LoginForm([
-            'username' => 'not_existing_username',
-            'password' => 'not_existing_password',
+            'username' => '',
+            'password' => '',
         ]);
-        expect('model should not login user', $model->login())->false();
-        expect('user should not be logged in', Yii::$app->user->isGuest)->true();
+        expect_not($model->validate());
     }
-    public function testLoginWrongPassword()
-    {
-        $model = new LoginForm([
-            'username' => 'bayer.hudson',
-            'password' => 'wrong_password',
-        ]);
-        expect('model should not login user', $model->login())->false();
-        expect('error message should be set', $model->errors)->hasKey('password');
-        expect('user should not be logged in', Yii::$app->user->isGuest)->true();
-    }
-    public function testLoginCorrect()
+
+    public function testCorrect()
     {
         $model = new LoginForm([
             'username' => 'bayer.hudson',
             'password' => 'password_0',
         ]);
-        expect('model should login user', $model->login())->true();
-        expect('error message should not be set', $model->errors)->hasntKey('password');
-        expect('user should be logged in', Yii::$app->user->isGuest)->false();
+        expect_that($model->validate());
     }
-
 }
