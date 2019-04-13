0502a8c6ed25187589a8b3b2858c5575b7df252c
Victor Smirnov <istern24@mail.ru>
2019-04-13 13:45:43 +0300

    Renamed user table

--- core/entities/User/User.php
+++ core/entities/User/User.php
@@ -83,7 +83,7 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function tableName()
     {
-        return '{{%user}}';
+        return '{{%users}}';
     }
 
     /**
