ac897f1c9533674ae24aeeecfa6a7b6b4c638786
Victor Smirnov <istern24@mail.ru>
2019-04-15 19:53:48 +0300

    Removed unused rules

--- core/entities/User/User.php
+++ core/entities/User/User.php
@@ -139,15 +139,6 @@ class User extends ActiveRecord implements IdentityInterface
         ];
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public function rules()
-    {
-        return [
-            ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_WAIT]],
-        ];
-    }
 
     /**
      * {@inheritdoc}
